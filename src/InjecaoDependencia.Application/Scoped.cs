ca47901fe8a3b9a39e7d72cce5123ce5861f7a7e
Joao Paulo Moreira Antunes <joaopaulo.m@terra.com.br>
2022-08-17 21:34:50 -0300

    initial

---
+++ src/InjecaoDependencia.Application/Scoped.cs
@@ -0,0 +1,18 @@
+﻿using InjecaoDependencia.Application.Interfaces;
+
+namespace InjecaoDependencia.Application
+{
+    public class Scoped : IScoped
+    {
+        private readonly Guid valor;
+        public Scoped()
+        {
+            valor = Guid.NewGuid();
+        }
+
+        public Guid ObterScoped()
+        {
+            return valor;
+        }
+    }
+}
