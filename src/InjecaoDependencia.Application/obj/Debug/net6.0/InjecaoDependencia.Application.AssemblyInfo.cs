849617d2d8c378caec9b91edc918ee9bb9b292f4
joaopma <joaopaulo.m@terra.com.br>
2023-09-25 23:09:09 -0300

    sync

--- src/InjecaoDependencia.Application/obj/Debug/net6.0/InjecaoDependencia.Application.AssemblyInfo.cs
+++ src/InjecaoDependencia.Application/obj/Debug/net6.0/InjecaoDependencia.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InjecaoDependencia.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c10bc7f6fb8f955842f8ed9613c0b27d673614d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InjecaoDependencia.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InjecaoDependencia.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
